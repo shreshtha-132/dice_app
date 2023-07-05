292678a8991f00f7879e0feaf66780c29b120396
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-05 19:58:43 +0530

    updated main.dart
    ->expanded
    ->flex property

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +6,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.purple,
         appBar: AppBar(
           title: Text("dice"),
           backgroundColor: Colors.red,
@@ -21,6 +22,31 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset('images/dice1.png'),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset('images/dice2.png'),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
