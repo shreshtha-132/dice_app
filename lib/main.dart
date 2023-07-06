c0c0c9a50cab2a7ba8ffecc8eeca354c3a527532
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-06 11:09:54 +0530

    final code.
    -> stateful widget
    -> math library used to genrerate a random numbe between 1-6 to update the dice images on clicking.
    -> made a change dice face function to change the face of both the die if anyone of the image was clicked.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -17,8 +18,23 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+  // const DicePage({Key? key}) : super(key: key);
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +47,10 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
-                child: Image.asset('images/dice1.png'),
-                onPressed: () {},
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  changeDiceFace();
+                },
               ),
             ),
           ),
@@ -40,8 +58,10 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
-                child: Image.asset('images/dice2.png'),
-                onPressed: () {},
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  changeDiceFace();
+                },
               ),
             ),
           )
